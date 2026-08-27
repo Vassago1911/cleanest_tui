c1c0a558a746a7d12e8525b432222a29d8efe77f
ML <marc@lange-data.org>
2026-08-28 00:02:41 +0200

    removed duplicate modules.rst from gitted files too

--- gen_documentation.sh
+++ gen_documentation.sh
@@ -10,3 +10,5 @@ gen_doc 1&> doc.log 2&> doc.log
 # now you can open the _build/index.html in a browser of your choice, like firefox
 got_warnings=0
 if [ $(grep WARNING doc.log | wc -l) -lt 1 ]; then got_warnings=0; else got_warnings=1; fi
+
+if [ "$got_warnings" -lt 1 ]; then clear; fi
