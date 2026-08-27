172103a6453b5289a542c22a7564a194fb804ce8
ML <marc@lange-data.org>
2026-08-27 23:30:10 +0200

    hopefully make submodules sphinx-discoverable with __init__.py s .. oldschool

--- conf.py
+++ conf.py
@@ -7,9 +7,9 @@ import os
 import sys
 
 # Setze den Pfad auf den lib-Ordner (relativ zur conf.py)
-sys.path.insert(0, os.path.abspath("../lib"))
+sys.path.insert(0, os.path.abspath("./lib"))
 # Falls deine Ordnerstruktur direkt im Hauptverzeichnis ist:
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
